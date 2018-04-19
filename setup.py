401a09869a67db1221116240851bff1e5ce3f990
Kordishal <jonaswaeber@gmail.com>
2018-04-19 09:31:11 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='simple-elastic',
+    packages=['simple-elastic'],
+    version='1.0',
+    description='',
+    author='Jonas Waeber',
+    author_email='jonaswaeber@gmail.com',
+    install_requires=['elasticsearch'],
+    url='https://github.com/UB-UNIBAS/simple-elastic',
+    download_url='',
+    keywords=['elasticsearch', 'elastic'],
+    classifiers=[],
+    license='MIT'
+)
