3109bd9e7b193a4a3af43f93a8eb634c73393217
Kordishal <jonaswaeber@gmail.com>
2018-04-19 09:42:26 +0200

    Added download url

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='simple-elastic',
     packages=['simple-elastic'],
-    version='1.0',
+    version='v0.1',
     description='A simple wrapper for the elasticsearch package.',
     author='Jonas Waeber',
     author_email='jonaswaeber@gmail.com',
     install_requires=['elasticsearch'],
     url='https://github.com/UB-UNIBAS/simple-elastic',
-    download_url='',
+    download_url='https://github.com/UB-UNIBAS/simple-elastic/archive/v0.1.tar.gz',
     keywords=['elasticsearch', 'elastic'],
     classifiers=[],
     license='MIT'
