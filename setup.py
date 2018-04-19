6882a18e63406f6d1258f9234433b85da504be5b
Kordishal <jonaswaeber@gmail.com>
2018-04-19 11:55:14 +0200

    Update version input 2.0

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from .selastic import __version__
+import os
+import re
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', read('selastic/__init__.py'), re.MULTILINE).group(1)
 
 setup(
     name='simple-elastic',
     packages=['selastic'],
-    version=__version__,
+    version=version,
     description='A simple wrapper for the elasticsearch package.',
     author='Jonas Waeber',
     author_email='jonaswaeber@gmail.com',
     install_requires=['elasticsearch'],
     url='https://github.com/UB-UNIBAS/simple-elastic',
-    download_url='https://github.com/UB-UNIBAS/simple-elastic/archive/v' + __version__ + '.tar.gz',
+    download_url='https://github.com/UB-UNIBAS/simple-elastic/archive/v' + version + '.tar.gz',
     keywords=['elasticsearch', 'elastic'],
     classifiers=[],
     license='MIT'
