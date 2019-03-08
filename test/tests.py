bc4f86183b06c4bda6de5fbf7a099152d57923cb
Jonas Waeber <jonaswaeber@gmail.com>
2019-03-08 09:51:20 +0100

    test(index): Add two tests for search.

--- test/tests.py
+++ test/tests.py
@@ -3,8 +3,8 @@ from simple_elastic import ElasticIndex
 import sys
 import logging
 
-
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
 
 class TestElasticIndex(object):
 
@@ -25,10 +25,19 @@ class TestElasticIndex(object):
     def test_index_into(self):
 
         result = self.index.index_into({'test': True, 'object': "This is a string"}, 5)
-        assert result == True
+        assert result
         result = self.index.index_into({'test': True, 'object': {'sub-object': "another string"}}, 6)
-        assert result == False
+        assert not result
         result = self.index.index_into({'test': False}, 'HAN000827182')
-        assert result == True
+        assert result
 
-    
+    def test_search(self):
+        data = list()
+        data.append({'id': '1234', 'test': True})
+        self.index.bulk(data=data, identifier_key='id')
+        result = self.index.search()
+        assert len(result) == 7
+
+    def test_search_not_unpack(self):
+        result = self.index.search(unpack=False)
+        assert len(result) == 7
